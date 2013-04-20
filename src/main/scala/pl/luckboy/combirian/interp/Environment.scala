52e7db01e44b930c3ed8a30647c7f90991f0a596
Lukasz Szpakowski <luckboy@vp.pl>
2013-04-20 15:17:29 +0200

    The Environment class shouldn't be case class.

--- src/main/scala/pl/luckboy/combirian/interp/Environment.scala
+++ src/main/scala/pl/luckboy/combirian/interp/Environment.scala
@@ -1,12 +1,23 @@
 package pl.luckboy.combirian.interp
 import scala.collection.immutable.IntMap
 
-case class Environment(globalVarValues: IntMap[Value], localVarValues: Array[Value]) extends EnvironmentLike[Environment]
+class Environment(values1: IntMap[Value], values2: Array[Value]) extends EnvironmentLike[Environment]
 {
+  override protected val globalVarValues = values1
+  override protected val localVarValues = values2
+  
   override protected def createEnv(maxLocalVarCount: Int): Environment = {
     val newLocalVarValues = Array.fill(maxLocalVarCount)(ErrorValue("undefined local variable", Seq()): Value)
-    Environment(globalVarValues, newLocalVarValues)
+    new Environment(globalVarValues, newLocalVarValues)
   }
   
-  override def withGlobalVars(values: Map[Int, Value]) = Environment(globalVarValues ++ values, localVarValues)
+  override def withGlobalVars(values: Map[Int, Value]) = new Environment(globalVarValues ++ values, localVarValues)
+}
+
+object Environment
+{
+  def empty = Environment(IntMap(), Seq())
+  
+  def apply(globalVarValues: IntMap[Value], localVarValues: Seq[Value]) =
+    new Environment(globalVarValues, localVarValues.toArray)
 }
