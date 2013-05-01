d1350d0b169e4b59dc8c89feb736b0eef5ef67f0
Lukasz Szpakowski <luckboy@vp.pl>
2013-05-02 00:07:27 +0200

    Added some tests.

--- src/test/scala/pl/luckboy/combirian/interp/spec/InterpreterSpec.scala
+++ src/test/scala/pl/luckboy/combirian/interp/spec/InterpreterSpec.scala
@@ -38,6 +38,59 @@ f1 x y = x + y
 g4 = f2 10 4 // (10 * 10) + (4 * 4) + 5 = 121
 """, "Some thing") should be ===(Right("139\n"))
     }
+    
+    it should "interpret a program with a condition" in {
+      val s = "main = \\x -> if(x == \"yes\") (\"yes!\", nil) else (\"no!\", nil)"
+      interpString(s, "yes") should be ===(Right("yes!\n"))
+      interpString(s, "no") should be ===(Right("no!\n"))
+    }
+    
+    it should "interpret a program with a partial application" in {
+      val s = """
+f x y z = x + y + z
+g x = f x x
+main = \x -> (g 10 (intfrom x), nil)
+"""
+      interpString(s, "5") should be ===(Right("25\n"))
+      interpString(s, "100") should be ===(Right("120\n"))
+    }
+    
+    it should "allocate local variables" in {
+      val s = """
+main = \x -> let
+    x1 = x + x
+    x2 = (intfrom x) * ((intfrom x) + 1)
+  in
+    let
+      x3 = x1 + "!"
+      x4 = x2 / 10
+    in
+      (x1 + "," + (stringfrom x2) + "," + x3 + "," + (stringfrom x4), nil)
+"""
+      interpString(s, "3") should be ===(Right("33,12,33!,1\n"))
+      interpString(s, "10") should be ===(Right("1010,110,1010!,11\n"))
+    }
+    
+    it should "create a copy of the array with a new element in non-lambda expression" in {
+      interpString("""
+main = \x -> let
+    a = #[1, 2, 3, 4]
+  in
+    ((stringfrom a) + "," + (stringfrom (updated 1 10 a)) + "," + (stringfrom (updated 2 15 a)), nil)
+""", "Some thing") should be ===(Right("#[1, 2, 3, 4],#[1, 10, 3, 4],#[1, 2, 15, 4]\n"))
+    }
+
+    it should "create a copy of the array with a new element in lambda expression" in {
+      interpString("""
+main = \x -> let
+    a = #[1, 2, 3, 4]
+  in
+    let
+      f = \v -> updated 2 v a 
+    in
+      ((stringfrom (f 10)) + "," + (stringfrom (f 15)), nil)
+""", "Some thing") should be ===(Right("#[1, 2, 10, 4],#[1, 2, 15, 4]\n"))
+    }
   }
   
   "A interpreter for an eager evaluator" should behave like interpreter(EagerEvaluator)(Environment)
